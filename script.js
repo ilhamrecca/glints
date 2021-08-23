36246e04600223677b2285053a8baf5725d04663
Muhammad Ilham Maulana <ilhamrecca@gmail.com>
2021-08-23 20:52:07 +0800

    coba

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+// TOGGLE
+
+const toggle = () => {
+
+	let toggle = document.querySelector(".toggle");
+	toggle.classList.toggle("fa-toggle-on");
+
+	let darkMode = document.querySelector(".darkMode");
+	darkMode.classList.toggle("fa-toggle-on");
+
+	let element = document.body;
+	element.classList.toggle("dark-mode");
+
+}
+
+// const darkMode = () => {
+
+// 	let darkMode = document.querySelector(".darkMode");
+// 	darkMode.classList.darkMode("fa-toggle-on");
+
+// 	let element = document.body;
+// 	element.classList.toggle("dark-mode");
+
+// }
+
+
+
+
+
+
+
+
+
